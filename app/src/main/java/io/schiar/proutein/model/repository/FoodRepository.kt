83846c7a24e3c8a5d346b641d2d0defc2eb5b009
Giovani Schiar <giovanischiar@gmail.com>
2022-06-15 02:27:31 -0700

    Fetching model data from an API.

--- app/src/main/java/io/schiar/proutein/model/repository/FoodRepository.kt
+++ app/src/main/java/io/schiar/proutein/model/repository/FoodRepository.kt
@@ -1,36 +1,39 @@
 package io.schiar.proutein.model.repository
 
+import android.os.Handler
+import android.os.Looper
+import io.schiar.proutein.BuildConfig
 import io.schiar.proutein.model.Food
+import org.json.JSONObject
+import java.net.URL
+import kotlin.concurrent.thread
 
 class FoodRepository: FoodRepositoryInterface {
-    private val foods = listOf(
-        Food(
-            "Cod Filé",
-            113.0,
-            90.0,
-            20.0
-        ),
-        Food(
-            "Peru Moído",
-            112.0,
-            120.0,
-            26.0
-        ),
-        Food(
-            "Peito de Frango",
-            100.0,
-            120.0,
-            23.1
-        ),
-        Food(
-            "Atum Albacore",
-            85.0,
-            100.0,
-            21.0
-        )
-    )
+    private val url = "https://sheets.googleapis.com/v4/spreadsheets/${BuildConfig.SPREADSHEET_ID}/values/'Página1'!A:D1?&key=${BuildConfig.API_KEY}"
 
     override fun fetch(callback: (List<Food>) -> Unit) {
-        callback(foods)
+        thread {
+            val json = try {
+                URL(url).readText()
+            } catch (e: Exception) {
+                return@thread
+            }
+            val jsonObj = JSONObject(json)
+            val map = jsonObj.toMap()
+            @Suppress("UNCHECKED_CAST")
+            val values = map["values"] as List<List<String>>?
+            val foods = values?.subList(1, values.size)?.map {
+                Food(
+                    it[0],
+                    it[1].replace(',', '.').toDouble(),
+                    it[3].replace(',', '.').toDouble(),
+                    it[2].replace(',', '.').toDouble()
+                )
+            }
+
+            Handler(Looper.getMainLooper()).post {
+                callback(foods ?: listOf())
+            }
+        }
     }
 }
